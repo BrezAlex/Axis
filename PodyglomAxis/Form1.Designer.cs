1f037565ae756cae318bad4c86c8141ffd6db4f2
Alexandr Brezin <sasha.brezin7262@gmail.com>
2020-04-23 14:21:13 +0300

    Добавьте файлы проекта.

---
+++ PodyglomAxis/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PodyglomAxis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.axis1 = new myUCLib.Axis();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // axis1
+            // 
+            this.axis1.axis_bkcolor = System.Drawing.Color.White;
+            this.axis1.axis_color = System.Drawing.Color.Gray;
+            this.axis1.Axis_Type = ((byte)(1));
+            this.axis1.E_x = 0;
+            this.axis1.E_y = 0;
+            this.axis1.Location = new System.Drawing.Point(36, 134);
+            this.axis1.Name = "axis1";
+            this.axis1.Pix_color = System.Drawing.Color.Black;
+            this.axis1.Pix_Size = 2F;
+            this.axis1.Pix_type = ((byte)(1));
+            this.axis1.Size = new System.Drawing.Size(715, 289);
+            this.axis1.TabIndex = 0;
+            this.axis1.x_Base = 1F;
+            this.axis1.x_Name = "X";
+            this.axis1.y_Base = 1F;
+            this.axis1.y_Name = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.axis1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private myUCLib.Axis axis1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ PodyglomAxis/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PodyglomAxis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double x;
+            double y;
+            double t;
+            double speedX;
+            double speedY;
+            double g;
+            double a;
+            double k;
+
+        }
+    }
+}
