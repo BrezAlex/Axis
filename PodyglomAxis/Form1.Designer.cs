7b7f5b12dbaae1767379ee62f8525c3b7170ae1d
Alexandr Brezin <sasha.brezin7262@gmail.com>
2020-04-24 13:52:15 +0300

    Проверка

--- PodyglomAxis/Form1.Designer.cs
+++ PodyglomAxis/Form1.Designer.cs
@@ -32,36 +32,131 @@
             this.axis1 = new myUCLib.Axis();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // axis1
             // 
+            this.axis1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.axis1.axis_bkcolor = System.Drawing.Color.White;
             this.axis1.axis_color = System.Drawing.Color.Gray;
             this.axis1.Axis_Type = ((byte)(1));
             this.axis1.E_x = 0;
             this.axis1.E_y = 0;
-            this.axis1.Location = new System.Drawing.Point(36, 134);
+            this.axis1.Location = new System.Drawing.Point(12, 134);
             this.axis1.Name = "axis1";
             this.axis1.Pix_color = System.Drawing.Color.Black;
             this.axis1.Pix_Size = 2F;
             this.axis1.Pix_type = ((byte)(1));
-            this.axis1.Size = new System.Drawing.Size(715, 289);
+            this.axis1.Size = new System.Drawing.Size(776, 304);
             this.axis1.TabIndex = 0;
             this.axis1.x_Base = 1F;
             this.axis1.x_Name = "X";
             this.axis1.y_Base = 1F;
             this.axis1.y_Name = "Y";
+            this.axis1.Load += new System.EventHandler(this.axis1_Load);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Без сопротивления";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(288, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "С сопротивлением";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(288, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Стоп";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button3_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "30";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(162, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "20";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Начальная скорость";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Начальный угол";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.axis1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +165,13 @@
         private myUCLib.Axis axis1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- PodyglomAxis/Form1.cs
+++ PodyglomAxis/Form1.cs
@@ -1,29 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PodyglomAxis
 {
     public partial class Form1 : Form
     {
+
+
+        private double _ygol;
+        private double _firstSpeed;
+        private double _speedX;
+        private double _speedY;
+        private double _x;
+        private double _y;
+        private double _t;
         public Form1()
         {
-            InitializeComponent();
-            double x;
-            double y;
-            double t;
-            double speedX;
-            double speedY;
-            double g;
-            double a;
-            double k;
 
+            InitializeComponent();
+           
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _t += 0.05;
+            _x = (float) _speedX * _t;
+            _y = -(float) _speedY * _t - 9.81 * _t * _t / 20;
+            if (_y <= 0)
+            {
+                timer1.Stop();
+                _x = 0;
+                _y = 0;
+                _t = 0;
+            }
+            axis1.PixDraw((float) _x, (float) _y, Color.Blue, 0);
+            axis1.StatToPic();
+        }
+        private void axis1_Load(object sender, EventArgs e)
+        {
+            axis1.Axis_Type = 3;
+            axis1.x_Name = "Ox";
+            axis1.y_Name = "Oy";
+            axis1.x_Base = Width;
+            axis1.y_Base = Height;
+            axis1.Pix_Size = (float) 0.001;
+            axis1.AxisDraw();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            var vse = _ygol * Math.PI / 180;
+            double.TryParse(textBox1.Text, out _firstSpeed);
+            double.TryParse(textBox2.Text, out _ygol);
+            _speedX = (float)(_firstSpeed * Math.Cos(vse));
+            _speedY = (float)(_firstSpeed * Math.Sin(-vse));
+            timer1.Start();
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            var vse = _ygol * Math.PI / 180;
+            double.TryParse(textBox1.Text, out _firstSpeed);
+            double.TryParse(textBox2.Text, out _ygol);
+            _speedX = (float)(_firstSpeed * Math.Cos(vse));
+            _speedY = (float)(_firstSpeed * Math.Sin(-vse));
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            _t += 0.1;
+            _speedX -= 0.001 * _speedX * _speedX;
+            _speedY += 9.81 * 0.01;
+            _x += _speedX * 0.1;
+            _y -= _speedY * 0.1;
+            if (_y<=0)
+            {
+                timer2.Stop();
+                _x = 0;
+                _y = 0;
+                _t = 0;
+            }
+            axis1.PixDraw((float)_x, (float)_y, Color.Red, 0);
+            axis1.StatToPic();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar)) return;
+            if (e.KeyChar == Convert.ToChar(Keys.Back)) return;
+            e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar)) return;
+            if (e.KeyChar == Convert.ToChar(Keys.Back)) return;
+            e.Handled = true;
+        }
+
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
         }
     }
 }
